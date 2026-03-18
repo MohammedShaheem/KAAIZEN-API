f2a333a61e769baaf04887ab2e8602850562ae23
Mohammed Shaheem <mohammedshaheemtk2@gmail.com>
2026-03-18 15:38:49 +0530

    admin route  fixed

--- kaaizen/urls.py
+++ kaaizen/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dj-admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('', include('users.urls')),
     path('api/auth/',include('auth_api.urls')),
