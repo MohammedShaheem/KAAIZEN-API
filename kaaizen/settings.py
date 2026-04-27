b1cff7450402dc2c6da7b7c0ce10563fdd2cb2b8
Mohammed Shaheem <mohammedshaheemtk2@gmail.com>
2026-04-27 12:11:43 +0530

    fix:personal training wallet management

--- kaaizen/settings.py
+++ kaaizen/settings.py
@@ -106,10 +106,11 @@ DATABASES = {
         'NAME': config('DB_NAME'),        
         'USER': config('DB_USER'),           
         'PASSWORD': config('DB_PASSWORD'),   
-        'HOST': 'localhost',          
+        'HOST': config('DB_HOST'),          
         'PORT': config('DB_PORT'),               
     }
 }
+# 'localhost'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
